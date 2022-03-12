d1eee6f274944a8426e413ed89a489ffc814436a
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-03-12 22:54:46 +0000

    #3 Add input function.

--- src/NewFlat.tsx
+++ src/NewFlat.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewFlat = () => {
+  const [name, setName] = useState("");
+  const handleInputName = (e: any) => {
+    e.preventDefault();
+    setName(e.target.value);
+  };
+
+  const handleCreateButton = () => {};
+
   return (
     <>
       <h1>New Flats</h1>
       Name
-      <input />
+      <input
+        placeholder="Enter the name"
+        value={name}
+        onChange={handleInputName}
+      />
+      {console.log(name)}
       Status
       <input />
       Image
       <input />
       Content
       <input />
-      <button>Create</button>
+      <button onClick={handleCreateButton}>Create</button>
     </>
   );
 };
