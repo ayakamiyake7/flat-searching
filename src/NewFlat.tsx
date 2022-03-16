375dca9720fcce74f5e0eca05041e82d4c012b77
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-03-16 22:17:35 +0000

    #7 Add function of images uplaoding.

--- src/NewFlat.tsx
+++ src/NewFlat.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ImageUploading from "react-images-uploading";
 
 const NewFlat = () => {
   const [name, setName] = useState("");
@@ -14,6 +15,13 @@ const NewFlat = () => {
     console.log(status);
   };
   console.log(status);
+
+  const [images, setImages] = useState([]);
+  const handleChangeImages = (imageList: any, addUpdateIndex: any) => {
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
   const handleCreateButton = () => {};
 
   return (
@@ -33,7 +41,44 @@ const NewFlat = () => {
         <option value="Not Accepted">Not Accepted</option>
       </select>
       Image
-      <input />
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={handleChangeImages}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div>
+            <button
+              style={isDragging ? { color: "red" } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image["data_url"]} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
       Content
       <input />
       <button onClick={handleCreateButton}>Create</button>
