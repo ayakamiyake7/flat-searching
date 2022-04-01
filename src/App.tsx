9fa5822fd758b60e01bd5458be1a659d935ad280
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-04-01 09:55:59 +0100

    #14 Output data.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import "./App.css";
 import AddButton from "./components/atoms/buttons/AddButton";
 
 function App() {
+  const handleClickDetail = () => {};
+
   return (
     <>
       <p>Flat Searching</p>
       <AddButton />
+
+      <h1>Flat List</h1>
+      <section>
+        <img src="" />
+        {/* <span>{status}</span> */}
+        {/* <h2>{name}</h2> */}
+        <button onClick={handleClickDetail}>Detail</button>
+      </section>
     </>
   );
 }
