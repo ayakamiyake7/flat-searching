6540e74fbb734580acf6cc0af0387f94dd9ba627
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-04-07 05:54:02 +0100

    #15 Design posts page.

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -4,10 +4,27 @@ const Posts = (props: any) => {
   const { id, name, status, images, content } = props;
   console.log(props);
   return (
-    <div>
-      Posts page
-      <p>This is the edit page.</p>
-    </div>
+    <>
+      <button>Edit</button>
+      <section>
+        <h1>Name</h1>
+        <span>Status</span>
+        <section>
+          <h2>Image gallery</h2>
+          <div></div>
+        </section>
+        <section>
+          <h2>Content</h2>
+          <p>text</p>
+        </section>
+        <section>
+          <h2>Review</h2>
+          <div>1</div>
+          <div>2</div>
+          <button>Add Review</button>
+        </section>
+      </section>
+    </>
   );
 };
 
