0324216c6b81eead654d89c02531884731946aa8
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-04-08 20:29:21 +0100

    #31 Fix name.

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,31 +2,35 @@ import React from "react";
 import AddButton from "./components/atoms/buttons/AddButton";
 import { Items } from "./components/types/items";
 import { useRecoilState } from "recoil";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { flatState } from "./components/states/flatState";
 import { detailState } from "./components/states/detaiState";
 
 const Main = () => {
   const [flats, setFlats] = useRecoilState(flatState);
   const setDetailFlat: any = useRecoilState(detailState);
+  const flat = useRecoilState(detailState);
 
-  const handleClickDetail = (selectedFlat: any) => {
-    // setDetailFlat(selectedFlat);
-  };
-  // console.log(setDetailFlat);
+  const HandleClickDetail = (flat: Items) => {};
 
   return (
     <>
       <p>Flat Searching</p>
       <AddButton />
       <h1>Flat List</h1>
-      {flats.map((flat: Items) => (
+      {flats.map((flat: Items, index) => (
         <section key={flat.id}>
           <img src={flat.images[0]["data_url"]} alt="" width="100" />
           <span>{flat.status}</span>
           <h2>{flat.name}</h2>
-          <Link to={`posts/${flat.id}`}>
-            <button onClick={handleClickDetail}>Detail</button>
+          <Link to={`flat/${flat.id}`} state={{ id: flat.id }}>
+            <button
+              onClick={() => {
+                HandleClickDetail(flat);
+              }}
+            >
+              Detail
+            </button>
           </Link>
         </section>
       ))}
