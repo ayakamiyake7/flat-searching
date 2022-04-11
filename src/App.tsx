ca0e347ad765d2dd8ad091d38838d78244761f30
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-04-11 16:23:53 +0100

    #34 Try to pass props.

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { flatState } from "./components/states/flatState";
 import { detailState } from "./components/states/detaiState";
 
-function App() {
+function App(props: any) {
   // const [flats, setFlats] = useRecoilState(flatState);
   // console.log("App.tsx =", flats);
   // const flat = useRecoilState(detailState);
+  console.log(props);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="NewFlat" element={<NewFlat />} />
-        <Route path="/Flat/:id" element={<Flat name="{props.name}" />} />
+        {/* <Route path="/Flat/:id" element={<Flat name="{props.name}" />} /> */}
+        <Route path="/Flat/:id" element={<Flat name={props.name} />} />
       </Routes>
     </BrowserRouter>
   );
